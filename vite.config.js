fd5037f40f833f7b577b0494bd0ae6ff943f8fcb
chong <c22425688@gmail.com>
2026-02-11 11:50:07 +0800

    Day 43

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 加入這一行，確保打包後的資源路徑指向你的 GitHub 倉庫名稱
+  base: '/react-daily-task-router-vite/',
   plugins: [react()],
 })
