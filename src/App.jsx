8b2abd478bf8b1c2c27b8b01997d4455e5361493
chong <c22425688@gmail.com>
2026-02-12 16:14:46 +0800

    Day 44

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
 import { HashRouter, NavLink } from 'react-router-dom';
 import { Route,Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return (
+    <>
+    <p>這是 Todo 頁面</p>
+    <LogOut/>
+    </>
+  )
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
 };
+const LogOut = () => {
+  const  navigate = useNavigate();
+  return <button onClick={() => {navigate('/login')}}>登出</button>
+}
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
